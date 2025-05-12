93558fbdcbfa8a0998db970c73cb272f1fc41afc
Shardz689 <shardul07@zoommylife.com>
2025-05-12 22:04:26 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -46,10 +46,10 @@ except ImportError:
 from dotenv import load_dotenv
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "YOUR_GEMINI_API_KEY_PLACEHOLDER")
-NEO4J_URI = os.getenv("NEO4J_URI", "YOUR_NEO4J_URI_PLACEHOLDER")
-NEO4J_USER = os.getenv("NEO4J_USER", "YOUR_NEO4J_USER_PLACEHOLDER")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "YOUR_NEO4J_PASSWORD_PLACEHOLDER")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBv-I8Ld-k09Lxu9Yi7HPffZHKXIqGSdHU")
+NEO4J_URI = os.getenv("NEO4J_URI", "neo4j+s://1b47920f.databases.neo4j.io")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "eCqDfyhDcuGMLzbYfiqL6jsvjH3LIXr86xQGAEKmY8Y")
 NEO4J_AUTH = (NEO4J_USER, NEO4J_PASSWORD)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
